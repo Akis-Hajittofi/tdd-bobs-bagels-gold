0333bbf496ea16675599abf90e90dee5855f0284
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-03-06 16:34:56 +0000

    Changed source code, mainly add and remove from basket and same for the tests

--- src/basket.js
+++ src/basket.js
@@ -1,24 +1,30 @@
-const inventory = require('./inventory.js')
+const inventory = require("./inventory.js");
 
 class Basket {
-  constructor() {
-    this.basket = [];
-    this.basketSize = 4;
+  constructor(basket = [], basketSize = 4) {
+    this.basket = basket;
+    this.basketSize = basketSize; // Not clear if this is for capacity or not / I think it's pretty clear. The size of the basket is the amount of thing that can fit, no?
   }
 
-  addBagelToBasket(type) {
-    for (let i = 0; i < inventory.length; i++){
-      const bagel = inventory[i]
-      if (bagel["variant"] === type && this.basket.length < this.basketSize) {
-        this.basket.push(bagel)
-        return
+  // Change to forEach and put more suitable parameter name
+  addToBasket(bagelSku) {
+    //if basket full, no need to do anything. Sending back null
+    if (this.basket.length >= this.basketSize) return null;
+
+    let i = 0;
+    let bagel;
+    //finding the bagel type from the inventory
+    inventory.forEach((element) => {
+      if (element["sku"] === bagelSku) {
+        bagel = element;
+        this.basket.push(bagel);
+        return;
       }
-    }
-    return "Basket is full!"
-     
-
+    });
   }
-  removeBagelFromBasket(type) {
+
+  removeFromBasket(bagelSku) {
+    // Use a .filter instead?
     for (let i = 0; i < this.basket.length; i++) {
       const bagelToRemove = this.basket[i];
       if (bagelToRemove["variant"] == type) {
@@ -26,47 +32,59 @@ class Basket {
         return;
       }
     }
+    // Return an object with a succes value and the item that was removed
     return "This Bagel doesn't exist";
   }
-  
+
   getTotalOfBasket() {
-      let count = 0;
-      for (let i = 0; i < this.basket.length; i++) {
-          const bagelPrice = this.basket[i]["price"]
-          count = count + bagelPrice
-      }
-      count = count - this.getDiscount()
-      
-      return Number(count.toFixed(2))
+    let count = 0;
+    for (let i = 0; i < this.basket.length; i++) {
+      const bagelPrice = this.basket[i]["price"];
+      count = count + bagelPrice;
+    }
+    count = count - this.getDiscount();
+
+    return Number(count.toFixed(2));
   }
 
   getDiscount() {
     let discount = 0;
-    const discountOnionArray = this.basket.filter(variant => variant["variant"] === "Onion")
-    const everythingBagelArray = this.basket.filter(variant => variant["variant"] === "Everything")
-    const plainBagelArray = this.basket.filter(variant => variant["variant"] === "Plain")
-    const discMultiplierOne = Math.trunc(discountOnionArray.length / 6)
-    const discMultiplierTwo = Math.trunc(everythingBagelArray.length / 6)
-    const discMultiplierThree = Math.trunc(plainBagelArray.length / 12)
+    // Change all of this into a single function
+    const discountOnionArray = this.basket.filter(
+      (variant) => variant["variant"] === "Onion"
+    );
+    const everythingBagelArray = this.basket.filter(
+      (variant) => variant["variant"] === "Everything"
+    );
+    const plainBagelArray = this.basket.filter(
+      (variant) => variant["variant"] === "Plain"
+    );
+
+    // Change all of this into a single function
+    const discMultiplierOne = Math.trunc(discountOnionArray.length / 6);
+    const discMultiplierTwo = Math.trunc(everythingBagelArray.length / 6);
+    const discMultiplierThree = Math.trunc(plainBagelArray.length / 12);
+
+    // Change all of this into a single function
     if (discountOnionArray.length >= 6) {
-      discount = discount + (discMultiplierOne*0.45)
+      discount = discount + discMultiplierOne * 0.45;
     }
-    if (everythingBagelArray.length >= 6)  {
-      discount = discount + (discMultiplierTwo*0.45)
+    if (everythingBagelArray.length >= 6) {
+      discount = discount + discMultiplierTwo * 0.45;
     }
     if (plainBagelArray.length >= 12) {
-      discount = discount + (discMultiplierThree*0.69)
+      discount = discount + discMultiplierThree * 0.69;
     }
-    return discount
+    return discount;
   }
 
   getBagelPrice(type) {
-      for (let i = 0; i < inventory.length; i++) {
-          const bagel = inventory[i]
-          if(bagel["variant"] === type) {
-              return inventory[i]
-          }
+    for (let i = 0; i < inventory.length; i++) {
+      const bagel = inventory[i];
+      if (bagel["variant"] === type) {
+        return inventory[i];
       }
+    }
   }
   getBasket() {
     return this.basket;
